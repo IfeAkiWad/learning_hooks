f610c2fc4b136f2f07771fe3f0056a054d69db4e
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-25 23:19:57 -0600

    learning custom hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react"
-import ClassComponentCounter from "./ClassComponentCounter";
-
+import useState from "./useState"
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <div>
-        this is a counter that was built using useState hook.
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 5)}>CLICK!</button>
-      <br /><br />
-        this is a counter built using a class Component
-        <ClassComponentCounter />
-    </div>
+        
+
+   </div>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <div>
+            <Link id="link" to="/hooks"></Link>
+
+        </div>
+    )
+}
+
+export default Navbar
