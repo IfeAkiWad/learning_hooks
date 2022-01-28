cd9c4d336421dbcece69c5505fe8f15c5e58586a
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-27 23:15:12 -0600

    learning useEffect hook

--- src/AgeCalculator.js
+++ src/AgeCalculator.js
@@ -13,11 +13,15 @@ import TimesThree from "./TimesThree";
       setCalculator(()=> age * 2)
     }, [age])
 
+    const resetAge = () => {
+      setAge("")
+    }
+
   return (
     <div>
       <h1>Age Calculator</h1>
       I'm learning how to use the useEffect hook<br /><br />
-      Enter age: <input type="text" value={age} onChange={handleOnChange} /><br /><br />
+      Enter age: <input type="text" value={age} onChange={handleOnChange} />&nbsp;<button onClick={resetAge}>Reset</button><br /><br />
       <h3>multiplied by two</h3>
       {/* <button onClick={setCalculator}>Age Calculator</button><br /><br /> */}
       result: <b>{calculator}</b><br /><br />
