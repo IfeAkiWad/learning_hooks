6bf5606584bd741b8fe620174c397de990bd5c8c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-09 17:38:56 -0600

    added Home page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AgeCalculator from './useStateHooks/AgeCalculator';
 import Timer from './useEffectHook/Timer';
 import Clock from './useEffectHook/Clock';
 import Vacation from './useContext/Vacation';
+import Home from './Home';
 
 class App extends Component {
     render() { 
@@ -15,7 +16,7 @@ class App extends Component {
                 <Router>
                 <NavBar />
                     <Routes>
-                    {/* <Route path="/" element={<Home />} />  */}
+                    <Route path="/learning_hooks/" element={<Home />} /> 
                     <Route path="/clickers" element={<UseStateHook />}/>
                     <Route path="/age-calculator" element={<AgeCalculator />}/>
                     <Route path="/timer" element={<Timer />}/>
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Welcome To Learning Hooks!</h1>
+        <h2>Decided to build as I learned React hooks.</h2>
+    </div>
+  )
+}
