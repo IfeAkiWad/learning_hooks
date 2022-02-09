d7042c20db6e48f0abfff989e9b4bdc4fc4af8a0
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-08 21:48:50 -0600

    learning useContext

---
+++ src/useContext/Vacation.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+
+function Vacation () {
+    const [james, setJames] = useState('Flight 262')
+
+    const liberia = () => {
+        <h1>Monrovia</h1>
+    }
+}
+export default Vacation
