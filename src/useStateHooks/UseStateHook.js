f84a7a47f315aa011f7f09fb45f151445c725b03
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-09 16:43:01 -0600

    general updates

--- src/useStateHooks/UseStateHook.js
+++ src/useStateHooks/UseStateHook.js
@@ -7,14 +7,13 @@ function UseStateHook() {
   const [count, setCount] = useState(0)
   return (
     <div>
-      <h1>Learning Hooks</h1>
       <h2>Clicker</h2>
-        this is a counter that was built using useState hook.
+        This is a counter built using useState hook.
         <h1>{count}</h1>
         <button onClick={() => setCount(count + 5)}>CLICK!</button><br /><br />
         <a id="github" href="https://github.com/IfeAkiWad/learning_hooks/blob/master/src/useStateHooks/UseStateHook.js" rel="noreferrer" target="_blank">Code</a>
       <br /><br />
-        this is a counter built using a class Component
+        This is a counter built using a class Component
         <ClassComponentCounter /><br />
         <a id="github" href="https://github.com/IfeAkiWad/learning_hooks/blob/master/src/useStateHooks/ClassComponentCounter.js" rel="noreferrer" target="_blank">Code</a>
         <Footer />
