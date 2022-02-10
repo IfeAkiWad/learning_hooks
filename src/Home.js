f5770931e56c5def64a90087425b620328a98e46
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-09 18:14:46 -0600

    general updates

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
+import ReactIcon from './assets/ReactIcon.png'
 
 export default function Home() {
   return (
     <div>
         <h1>Welcome To Learning Hooks!</h1>
-        <h2>Decided to build as I learned React hooks.</h2>
+        <h2>Decided to build as I learned React hooks.</h2><br />
+        <img className="home-react-icon" src={ReactIcon} alt="React Icon" />
+        React Hooks Snippets by Al Duncanson
         <Footer />
     </div>
   )
