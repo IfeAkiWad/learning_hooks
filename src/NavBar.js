27c1992d3d8bab74798c8ea189dfa713f395bb70
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-02-11 23:04:47 -0600

    moblie friendly drop down menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <nav id="nav">
-        <div>    
+        <nav className="dropdown-nav">
+            <button class="dropbtn">Dropdown</button>
+            <div className="dropdown-content">>   
                 <Link id="link" to="/vacation"> VACATION</Link> 
                 <Link id="link" to="/clickers"> CLICKERS</Link> 
                 <Link id="link" to="/age-calculator"> AGE CALCULATOR</Link> 
                 <Link id="link" to='/timer'> TIMER</Link> 
                 <Link id="link" to='/clock'> CLOCK</Link>
                 <Link id="link" to='/learning_hooks/'> HOME</Link> 
- 
-        </div>
+            </div>
     </nav>
     )
 }
